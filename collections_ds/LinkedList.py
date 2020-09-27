852072226c9dcc46201057f6f12b1cb8efa3dd1f
adetunjionafuwa <adetunjionafuwa@gmail.com>
2020-09-27 10:52:59 +0200

    Implement initial Linked list class with custom __repr__

--- collections_ds/LinkedList.py
+++ collections_ds/LinkedList.py
@@ -6,3 +6,29 @@ class Node:
 
 	def __repr__(self):
 		return self.data
+
+
+#Implement LinkedList Object
+class LinkedList:
+	def __init__(self, nodes = None):
+		self.head = None
+
+		if nodes is not None:
+			#Pass first element in array into the node
+			node = Node(data = nodes.pop(0))
+			self.head = node
+
+			#Handle remaining element in nodes
+			for elem in nodes:
+				node.next = Node(data = elem)
+				node = node.next
+
+	def __repr__(self):
+		nodes = []
+		node  = self.head
+		while node is not None:
+			nodes.append(str(node.data))
+			node = node.next
+		nodes.append('None')
+
+		return '->'.join(nodes)
